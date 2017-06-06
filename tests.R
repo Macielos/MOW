9d337050dedab140d775b231fdba53a91b439498
mikojnacki <mikojnacki@gmail.com>
2017-06-06 03:04:24 +0200

    Added books data and tests

--- tests.R
+++ tests.R
@@ -46,3 +46,39 @@ results <- list(
 #  collaborativeFiltering(ratings, 0.9, "IBCF", "Cosine",  "cross-validation", 5, 10, 3, "ratings"),
 #  collaborativeFiltering(ratings, 0.9, "IBCF", "Pearson", "cross-validation", 5, 10, 3, "ratings")
 )
+
+
+###############################
+
+# Mikolaj
+
+# Przygotowanie danych do testów - wybór userów, którzy wystawili więcej niż 100 ocen
+booksTemp <- read.csv("data/book-crossing/BX-Book-Ratings.csv", sep = ";")
+booksTemp <- booksTemp[!(booksTemp$Book.Rating==0),]
+booksMatrixTemp <- as(booksTemp, "realRatingMatrix")
+booksMatrix <- booksMatrixTemp[rowCounts(booksMatrixTemp) > 100,]
+books <- as(booksMatrix, "data.frame")
+
+length(unique(books$user)) # 205 takich userów
+
+results2 <- list(
+  collaborativeFiltering(books, 0.9, "UBCF", "Cosine",  "split",            1, 10, 5, "ratings"),
+  collaborativeFiltering(books, 0.9, "UBCF", "Pearson", "split",            1, 10, 5, "ratings"),
+  
+  collaborativeFiltering(books, 0.9, "UBCF", "Cosine",  "bootstrap",        5, 10, 5, "ratings"),
+  collaborativeFiltering(books, 0.9, "UBCF", "Pearson", "bootstrap",        5, 10, 5, "ratings"),
+  
+  collaborativeFiltering(books, 0.9, "UBCF", "Cosine",  "cross-validation", 5, 10, 5, "ratings"),
+  collaborativeFiltering(books, 0.9, "UBCF", "Pearson", "cross-validation", 5, 10, 5, "ratings"),
+  
+  collaborativeFiltering(books, 0.9, "UBCF", "Cosine",  "split",            1, 10, 10, "ratings"),
+  collaborativeFiltering(books, 0.9, "UBCF", "Pearson", "split",            1, 10, 10, "ratings"),
+  
+  collaborativeFiltering(books, 0.9, "UBCF", "Cosine",  "split",            1, 20, 10, "ratings"),
+  collaborativeFiltering(books, 0.9, "UBCF", "Pearson", "split",            1, 20, 10, "ratings"),
+  
+  collaborativeFiltering(books, 0.9, "IBCF", "Cosine",  "split",            1, 10, 5, "ratings"),
+  collaborativeFiltering(books, 0.9, "IBCF", "Pearson", "split",            1, 10, 5, "ratings"),
+  
+  collaborativeFiltering(books, 0.9, "UBCF", "Cosine",  "split",            1, 10, 5, "topNList"),
+  collaborativeFiltering(books, 0.9, "UBCF", "Pearson", "split",            1, 10, 5, "topNList")
